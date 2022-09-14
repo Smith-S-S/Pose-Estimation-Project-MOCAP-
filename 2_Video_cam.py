815464c2fde9f639b77804a47fbe392300d339ef
Smith-S-S <80092760+Smith-S-S@users.noreply.github.com>
2022-09-14 22:59:17 +0530

    2 cameras
    it helps to open 2 cameras connected to the computer at a time

---
+++ 2_Video_cam.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cam_0 = cv2.VideoCapture(0)
+cam_1 = cv2.VideoCapture(1)
+
+while True:
+    success_0, frame_0 = cam_0.read()
+    success_1, frame_1 = cam_1.read()
+
+    cv2.imshow("sm", frame_0)
+    cv2.imshow("sm1", frame_1)
+
+    key= cv2.waitKey(1)
+    if key == 32:
+        break
+
+
+
+
+
+"""cam_0.release()
+cam_1.release()
+cv2.destroyAllWindows()"""
